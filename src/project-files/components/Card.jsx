bab8f0bb35b87f6f57ef3bfd95d9021ee5568974
George Cornea <george.cornea@outlook.com>
2023-04-30 16:34:03 -0700

    Create Card.jsx and css styles

---
+++ src/project-files/components/Card.jsx
@@ -0,0 +1,55 @@
+import { useStore } from '../store/store';
+import styles from './Card.module.css';
+
+export const Card = ({ imageUrl, id, size }) => {
+  const swap = useStore((state) => state.swap);
+  const remove = useStore((state) => state.remove);
+  const resize = useStore((state) => state.resize);
+
+  return (
+    <li className={styles.cards_item} id={id}>
+      <div className='card'>
+        <div className=''>
+          <img src={imageUrl} alt='' className={`image_size_${size}`} />
+        </div>
+        <div className='card_content'>
+          <button
+            className='left'
+            id={id}
+            onClick={(e) => swap(e.target.id, 'left')}
+          >
+            {'<'}
+          </button>
+          <button
+            className='left'
+            id={id}
+            onClick={(e) => swap(e.target.id, 'right')}
+          >
+            {'>'}
+          </button>
+          <button
+            className='increase'
+            id={id}
+            onClick={(e) => resize(e.target.id, 'up')}
+          >
+            {'+'}
+          </button>
+          <button
+            className='decrease'
+            id={id}
+            onClick={(e) => resize(e.target.id, 'down')}
+          >
+            {'-'}
+          </button>
+          <button
+            className='delete'
+            id={id}
+            onClick={(e) => remove(e.target.id)}
+          >
+            {'X'}
+          </button>
+        </div>
+      </div>
+    </li>
+  );
+};
